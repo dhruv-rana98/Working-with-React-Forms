cadc27ce7422f8a37afb4f60845770b2576a4f13
Dhruv Rana <dhruvrana0000@gmail.com>
2022-06-21 20:16:00 +0530

    Handling the whole form state and disable the button based on the inputs state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,9 +2,12 @@ import { useState } from "react";
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
+  let formIsValid = false;
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
@@ -45,7 +48,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
